f28700690583a7e20e1ab0efbde8512d939a7bfe
MCOlivera <mervin.c.olivera@gmail.com>
2015-11-03 17:51:37 +0000

    Prints the keys and values of a GET query in an HTML table format.

--- server.c
+++ server.c
@@ -20,6 +20,7 @@ int requestType;
 char requestFile[100];
 char contents[10000];
 char header[1000];
+char table[10000];
 
 void readFile(char *filename){
 	FILE *fp;
@@ -38,17 +39,17 @@ void readFile(char *filename){
 		else if (strstr(filename, ".html") != NULL)
 			strcpy(header, "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n");
 
-		while(fgets(str, sizeof(str), fp) != NULL){
+		while(fgets(str, sizeof(str), fp) != NULL)
 				strcat(contents, str);
-		}
-	} else {
-		strcpy(header, "HTTP/1.1 404 Not Found\r\n");
-		strcpy(contents, "<!DOCTYPE html><html><head><title>Page Not Found</title></head><body><h1>404 Not Found</h1></body></html>\r\n");
-	}
-	
-	strcat(contents, "\0");
 
-	fclose(fp);
+		fclose(fp);
+	} else if (errno == ENOENT){
+		strcpy(header, "HTTP/1.1 404 Not Found\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n");
+		strcpy(contents, "<!DOCTYPE html><html><head><title>Page Not Found</title></head><body><h1>404 Not Found</h1></body></html>\r\n");
+	} else if (errno == EACCES){
+		strcpy(header, "HTTP/1.1 403 Forbidden\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n");
+		strcpy(contents, "<!DOCTYPE html><html><head><title>Forbidden</title></head><body><h1>403 Forbidden</h1></body></html>\r\n");
+	}
 }
 
 void sigchld_handler(int s)
@@ -73,19 +74,54 @@ void *get_in_addr(struct sockaddr *sa)
 }
 
 void analyzeRequest(char *request){
-	int i;
+	int i, j;
 	char *pch;
+	char temp[100];
+	char *ch1, *ch2;
+	char keys[100][100];
+	char values[100][100];
 	
 	pch = strtok (request, " \t\n");
-	for (i=0; pch != NULL; i++){
+	for (i=0; i<3; i++){
 		if (i == 0){
 			if (strcmp("GET", pch) == 0) requestType = GET_METHOD;
 			else if (strcmp("POST", pch) == 0) requestType = POST_METHOD;
 		} else if (i == 1){
 			strcpy(requestFile, pch);
 		}
-		pch = strtok (NULL, " \t\n");
+		if (i<2) pch = strtok (NULL, " \t\n");
 	}
+
+	pch = strtok(NULL, "\t\n");
+	for(i=0; pch != NULL; i++){		
+		strcpy(temp, pch);
+		strtok_r(temp, ":", &ch1);
+		strcpy(keys[i], temp);
+		strcat(keys[i], "\0");
+		strcpy(values[i], ch1);
+		strcat(values[i], "\0");
+		pch = strtok (NULL, "\t\n");
+	}
+
+	strcpy(table, "<html><head></head><body><table>");
+	printf("<table>\n");
+	for (j=0; j<i-1; j++){
+		printf("<tr>\n");
+		strcat(table, "<tr>");
+		printf("%s %s\n", keys[j], values[j]);
+		printf("<td>%s</td> <td>%s</td>\n", keys[j], values[j]);
+		strcat(table, "<td>");
+		strcat(table, keys[j]);
+		strcat(table, "</td><td>");
+		strcat(table, values[j]);
+		strcat(table, "</td>");
+		printf("</tr>\n");
+		strcat(table, "</tr>");
+	}
+	printf("</table>\n");
+	strcat(table, "</table></body></html>");
+
+	// printf("%s\n", table);
 }
 
 int main(int argc, char *argv[])
@@ -98,7 +134,6 @@ int main(int argc, char *argv[])
 	int yes=1;
 	char s[INET6_ADDRSTRLEN];
 	int rv;
-	char filename[100];
 	char req[MAXDATASIZE];
 	int numbytes;
 	char response[10000];
@@ -186,7 +221,7 @@ int main(int argc, char *argv[])
 			
 			req[numbytes] = '\0';
 
-			printf("server: received %s\n", req);
+			printf("server: received:\n%s\n", req);
 			
 			analyzeRequest(req);
 			
@@ -194,8 +229,8 @@ int main(int argc, char *argv[])
 				readFile(requestFile);
 				strcat(response, header);
 				strcat(response, contents);
-
-				printf("%s\n", response);
+				strcat(response, table);
+				printf("\nResponse:\n%s\n", response);
 				if (send(new_fd, response, strlen(response), 0) == -1)
 					perror("send");
 
